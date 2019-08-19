389434966bf7f85e2ff36ab107f643a72a6d0820
stCarolas <stcarolas@gmail.com>
2019-08-19 12:03:45 +0300

    add archive repo example

--- examples/github-archive-repo/src/main/java/com/github/stcarolas/enki/Launcher.java
+++ examples/github-archive-repo/src/main/java/com/github/stcarolas/enki/Launcher.java
@@ -1,21 +1,44 @@
 package com.github.stcarolas.enki;
 
-import com.github.stcarolas.enki.gitea.analyzers.GiteaMirrorAnalyzer;
+import static com.github.stcarolas.enki.core.functions.TakeFunction.take;
+import static com.github.stcarolas.enki.core.functions.FilterFunction.filter;
+import java.util.List;
+import java.util.stream.Collectors;
+import com.github.stcarolas.enki.gitea.provider.GiteaRepoProvider;
+import com.github.stcarolas.enki.github.archiverepo.GithubArchiveRepoHandler;
 import com.github.stcarolas.enki.github.provider.GitHubRepoProvider;
 import com.github.stcarolas.enki.logginganalyzers.LoggingAnalyzers;
 import com.typesafe.config.ConfigFactory;
 import lombok.Builder;
 import lombok.val;
-import static com.github.stcarolas.enki.core.functions.FilterFunction.filter;
+import lombok.extern.log4j.Log4j2;
 
 @Builder
+@Log4j2
 public class Launcher {
 
     public static void main(String[] args) {
         val config = ConfigFactory.load();
+        List<String> repos = new GiteaRepoProvider(
+            config.getString("gitea.url"),
+            config.getString("gitea.organization")
+        )
+            .getRepos()
+            .stream()
+            .map(
+                item -> {
+                    return item.getName();
+                }
+            )
+            .collect(Collectors.toList());
+        repos.forEach(
+            item -> {
+                log.info(item);
+            }
+        );
         EnkiServer.builder()
             .provider(
-                filter(repo -> repo.getName().contains("receipt-1c-service"))
+                filter(repo -> repos.contains(repo.getName()))
                     .from(
                         GitHubRepoProvider.builder()
                             .username(config.getString("github.username"))
@@ -25,6 +48,13 @@ public class Launcher {
                     )
             )
             .analyzer(new LoggingAnalyzers())
+            .analyzer(
+                GithubArchiveRepoHandler.builder()
+                    .username(config.getString("github.username"))
+                    .password(config.getString("github.password"))
+                    .organization(config.getString("github.organization"))
+                    .build()
+            )
             .serverHost("0.0.0.0")
             .port(8080)
             .build()
