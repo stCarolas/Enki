183af775bf833a9d404dd12b33461d5abead5ff6
stCarolas <stcarolas@gmail.com>
2019-08-22 22:54:39 +0300

    add one-repo provider

---
+++ modules/one-repo-provider/src/main/java/com/github/stcarolas/enki/gitea/provider/OneRepoProvider.java
@@ -0,0 +1,20 @@
+package com.github.stcarolas.enki.gitea.provider;
+
+import java.util.Arrays;
+import java.util.List;
+import com.github.stcarolas.enki.core.CloneURLType;
+import com.github.stcarolas.enki.core.Repo;
+import com.github.stcarolas.enki.core.RepoProvider;
+import com.github.stcarolas.enki.core.impl.GitRepo;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OneRepoProvider implements RepoProvider {
+    private final String cloneUrl;
+    private final CloneURLType urlType;
+
+    @Override
+    public List<Repo> getRepos() {
+        return Arrays.asList(GitRepo.builder().cloneUrl(urlType, cloneUrl).build());
+    }
+}
