fb1ace9191b9dc15849d3c5de6a8599fbe9846e9
stCarolas <stcarolas@gmail.com>
2020-02-05 21:36:26 +0300

    add checking for providers and handlers existance

--- core/src/main/java/com/github/stcarolas/enki/core/EnkiRunner.java
+++ core/src/main/java/com/github/stcarolas/enki/core/EnkiRunner.java
@@ -1,19 +1,23 @@
 package com.github.stcarolas.enki.core;
 
 import java.util.List;
+import java.util.Objects;
 
 import lombok.Builder;
-import lombok.Singular;
+import lombok.extern.log4j.Log4j2;
 
 @Builder
+@Log4j2
 public class EnkiRunner {
-    @Singular
     private List<RepoProvider> providers;
 
-    @Singular
     private List<RepoHandler> handlers;
 
     public void handle() {
+        if (Objects.isNull(providers) || Objects.isNull(handlers)){
+            log.info("no providers or handlers, exiting");
+            return;
+        }
         providers.stream()
             .flatMap(
                 provider -> {
