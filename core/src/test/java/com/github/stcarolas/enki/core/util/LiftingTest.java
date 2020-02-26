b1e113ec396c3130c78d2aed9518c165bdb8f2d7
stCarolas <stcarolas@gmail.com>
2020-02-26 15:36:26 +0300

    add test for Lifting utils

--- core/src/test/java/com/github/stcarolas/enki/core/util/LiftingTest.java
+++ core/src/test/java/com/github/stcarolas/enki/core/util/LiftingTest.java
@@ -1,5 +1,35 @@
 package com.github.stcarolas.enki.core.util;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.function.Supplier;
+
+import org.junit.jupiter.api.Test;
+
 public class LiftingTest {
+
+	@Test
+	public void test_return_value_as_option() {
+		var value = "value";
+		var result = Lifting.call(() -> value );
+		assertTrue(!result.isEmpty());
+		assertEquals(value, result.get());
+	}
+
+	@Test
+	public void test_return_null_as_none() {
+		String value = null;
+		var result = Lifting.call(() -> value );
+		assertTrue(result.isEmpty());
+	}
+
+	@Test
+	public void test_return_exception_as_none() {
+		var result = Lifting.call(
+			(Supplier<String>) () -> {throw new RuntimeException("some error");} 
+		);
+		assertTrue(result.isEmpty());
+	}
 	
 }
