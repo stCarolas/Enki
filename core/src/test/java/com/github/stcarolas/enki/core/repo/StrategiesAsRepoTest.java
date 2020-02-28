86cf9cb85ca749af3755084007499d65a0d8ccfa
stCarolas <stcarolas@gmail.com>
2020-02-28 16:46:46 +0300

    add tests for defaultRepoStrategies

--- core/src/test/java/com/github/stcarolas/enki/core/repo/StrategiesAsRepoTest.java
+++ core/src/test/java/com/github/stcarolas/enki/core/repo/StrategiesAsRepoTest.java
@@ -7,141 +7,144 @@ import java.util.function.Supplier;
 import com.github.stcarolas.enki.core.TestRepo;
 import static io.vavr.API.*;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class StrategiesAsRepoTest {
+	StrategiesAsRepo repo = new StrategiesAsRepo();
 
 	@Test
 	public void test_null_name_on_missing_strategy(){
-		var strategies = new StrategiesAsRepo();
-		Assertions.assertNull(strategies.name());
+		assertNull(repo.name());
 	}
 
 	@Test
 	public void test_null_name_if_strategy_provide_null_name(){
-		var strategies = new StrategiesAsRepo().setNameStrategy(() -> null);
-		Assertions.assertNull(strategies.name());
+		assertNull(
+			repo.setNameStrategy(() -> null).name()
+		);
 	}
 
 	@Test
 	public void test_null_name_if_strategy_falls(){
-		var strategies = new StrategiesAsRepo()
-			.setNameStrategy(() -> {throw new RuntimeException();});
-		Assertions.assertNull(strategies.name());
+		assertNull(
+			repo.setNameStrategy(() -> {throw new RuntimeException();}).name()
+		);
 	}
 
 	@Test
 	public void test_name_as_strategy_provides(){
 		var value = "value";
-		var strategies = new StrategiesAsRepo().setNameStrategy(() -> value);
-		Assertions.assertEquals(value, strategies.name());
+		assertEquals(
+			value, 
+			repo.setNameStrategy(() -> value).name()
+		);
 	}
 
 	@Test
 	public void test_null_id_on_missing_strategy(){
-		var strategies = new StrategiesAsRepo();
-		Assertions.assertNull(strategies.id());
+		assertNull(repo.id());
 	}
 
 	@Test
 	public void test_null_id_if_strategy_provide_null_name(){
-		var strategies = new StrategiesAsRepo().setIdentityStrategy(() -> null);
-		Assertions.assertNull(strategies.id());
+		assertNull(
+			repo.setIdentityStrategy(() -> null).id()
+		);
 	}
 
 	@Test
 	public void test_null_id_if_strategy_falls(){
-		var strategies = new StrategiesAsRepo()
-			.setIdentityStrategy(() -> {throw new RuntimeException();});
-		Assertions.assertNull(strategies.id());
+		assertNull(
+			repo.setIdentityStrategy(() -> {throw new RuntimeException();}).id()
+		);
 	}
 
 	@Test
 	public void test_same_id_as_strategy_provides(){
 		var value = "value";
-		var strategies = new StrategiesAsRepo().setIdentityStrategy(() -> value);
-		Assertions.assertEquals(value, strategies.id());
+		assertEquals(
+			value, 
+			repo.setIdentityStrategy(() -> value).id()
+		);
 	}
 
 	@Test
 	public void test_null_directory_on_missing_strategy(){
-		var strategies = new StrategiesAsRepo();
-		Assertions.assertNull(strategies.directory());
+		assertNull(repo.directory());
 	}
 
 	@Test
 	public void test_null_directory_if_strategy_provide_null_name(){
-		var strategies = new StrategiesAsRepo().setDirectoryStrategy(() -> null);
-		Assertions.assertNull(strategies.directory());
+		assertNull(
+			repo.setDirectoryStrategy(() -> null).directory()
+		);
 	}
 
 	@Test
 	public void test_null_directory_if_strategy_falls(){
-		var strategies = new StrategiesAsRepo()
-			.setDirectoryStrategy(() -> {throw new RuntimeException();});
-		Assertions.assertNull(strategies.directory());
+		assertNull(
+			repo.setDirectoryStrategy(() -> {throw new RuntimeException();}).directory()
+		);
 	}
 
 	@Test
 	public void test_same_directory_as_strategy_provides(){
 		var file = new File("/tmp/test");
-		var strategies = new StrategiesAsRepo()
-			.setDirectoryStrategy(() -> file);
-		Assertions.assertEquals(file,strategies.directory());
+		assertEquals(
+			file,
+			repo.setDirectoryStrategy(() -> file).directory()
+		);
 	}
 
 	@Test
 	public void test_return_null_on_missing_commit_strategy(){
-		var strategies = new StrategiesAsRepo();
-		Assertions.assertNull(strategies.directory());
+		assertNull(repo.directory());
 	}
 
 	@Test
 	public void test_return_null_if_commit_strategy_falls(){
-		var strategies = new StrategiesAsRepo()
-			.setCommitStrategy( arg -> {throw new RuntimeException();} );
-		Assertions.assertNull(strategies.commit("some arg"));
+		assertNull(
+			repo.setCommitStrategy( arg -> {throw new RuntimeException();} ).commit("some arg")
+		);
 	}
 
 	@Test
 	public void test_return_null_if_commit_strategy_provide_null_repo(){
-		var strategies = new StrategiesAsRepo()
-			.setCommitStrategy( arg -> null );
-		Assertions.assertNull(strategies.commit("some arg"));
+		assertNull(
+			repo.setCommitStrategy( arg -> null ).commit("some arg")
+		);
 	}
 
 	@Test
 	public void test_calling_commit_strategy_with_right_args(){
-		var repo = new TestRepo();
+		var testRepo = new TestRepo();
 		var message = "some message";
 
 		var strategy = mock(Function.class);
-		when(strategy.apply(any())).thenReturn(repo);
+		when(strategy.apply(any())).thenReturn(testRepo);
 
-		var strategies = new StrategiesAsRepo().setCommitStrategy(strategy);
-		Assertions.assertEquals(repo, strategies.commit(message));
+		assertEquals(testRepo, repo.setCommitStrategy(strategy).commit(message));
 		verify(strategy).apply(message);
 	}
 
 	@Test
 	public void test_return_empty_seq_on_missing_providers_strategy(){
-		var strategies = new StrategiesAsRepo();
-		Assertions.assertTrue(strategies.providers().isEmpty());
+		assertTrue(repo.providers().isEmpty());
 	}
 
 	@Test
 	public void test_return_empty_seq_if_providers_strategy_falls(){
-		var strategies = new StrategiesAsRepo()
-			.setProvidersStrategy( () -> {throw new RuntimeException();} );
-		Assertions.assertTrue(strategies.providers().isEmpty());
+		assertTrue(
+			repo.setProvidersStrategy( () -> {throw new RuntimeException();})
+				.providers().isEmpty()
+		);
 	}
 
 	@Test
 	public void test_return_empty_seq_if_provider_strategy_provide_null(){
-		var strategies = new StrategiesAsRepo().setProvidersStrategy( () -> null );
-		Assertions.assertTrue(strategies.providers().isEmpty());
+		assertTrue(repo.setProvidersStrategy( () -> null ).providers().isEmpty());
 	}
 
 	@Test
@@ -149,8 +152,10 @@ public class StrategiesAsRepoTest {
 		var strategy = mock(Supplier.class);
 		var providers = Seq();
 		when(strategy.get()).thenReturn(providers);
-		var strategies = new StrategiesAsRepo().setProvidersStrategy(strategy);
-		Assertions.assertEquals(providers, strategies.providers());
+		assertEquals(
+			providers, 
+			repo.setProvidersStrategy(strategy).providers()
+		);
 	}
 
 }
