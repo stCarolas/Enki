74850703a85f68bc0c15492fb94ccda61b3157c8
stCarolas <stcarolas@gmail.com>
2020-07-16 19:49:19 +0300

    fix test

--- handlers/shell-runner/src/test/java/com/github/stcarolas/enki/shell/ShellRunnerTest.java
+++ handlers/shell-runner/src/test/java/com/github/stcarolas/enki/shell/ShellRunnerTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ShellRunnerTest {
 	@Test
 	public void testExecuteShellCommand() {
-		assertTrue(new ShellRunner("/tmp/test.sh").apply(new File("/tmp/")).isSuccess());
+		assertTrue(new ShellRunner("echo test").apply(new File("/tmp/")).isSuccess());
 	}
 	
 }
